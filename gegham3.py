acb6b87959f6861e5e3916634334cfbdd183acd0
gegham14 <157041855+gegham14@users.noreply.github.com>
2024-03-19 18:14:28 +0400

    Add files via upload

---
+++ gegham3.py
@@ -0,0 +1,24 @@
+import threading
+
+
+def print_even_numbers():
+    print("List of even numbers:")
+    for i in range(0, 51, 2):
+        print(i, end=" ")
+
+
+def print_odd_numbers():
+    print("\nList of odd numbers:")
+    for i in range(1, 51, 2):
+        print(i, end=" ")
+
+
+# Create threads for printing even and odd numbers
+even_thread = threading.Thread(target=print_even_numbers)
+odd_thread = threading.Thread(target=print_odd_numbers)
+# Start the threads
+even_thread.start()
+odd_thread.start()
+# Wait for the threads to complete
+even_thread.join()
+odd_thread.join()
